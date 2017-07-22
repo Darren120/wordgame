072bb5ee271f66e506b230cfd9f26c4471c156f1
Darren Zou <zoudarrenn7@gmail.com>
2017-07-22 09:50:06 -0230

    Initial Commit

---
+++ swiftyWords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftyWords
+//
+//  Created by Darren on 7/22/17.
+//  Copyright © 2017 Darren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
